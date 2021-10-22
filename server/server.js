7a5070fece52644a2343eaead60746cbcf2f59c3
Eugene <evsyrovatka@gmail.com>
2021-10-22 14:09:08 +0300

    registr_settings

--- server/server.js
+++ server/server.js
@@ -2,19 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const port = 3001;
 app.use(express.json());
 app.use(cors());
 require("./src/routes")(app);
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 connectDb()
-  .then(() => console.log("mongoose connect"))
+  .then(() => console.log(`mongoose connect ${process.env.PORT}`))
   .catch((err) => console.log(err));
 
 async function connectDb() {
   await mongoose.connect("mongodb://localhost:27017/test");
 }
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 });
--- server/src/routes/user_router.js
+++ server/src/routes/user_router.js
@@ -1,4 +1,14 @@
 const User = require("../models/userModel");
+const { createHmac } = require("crypto");
+const jwt = require("jsonwebtoken");
+const secret = "fitnessTracker";
+const hash = createHmac("sha256", secret)
+  .update("fitnessTracker")
+  .digest("hex");
+
+function generateAccessToken(name) {
+  return jwt.sign(name, hash, { expiresIn: "1800s" });
+}
 
 module.exports = function (app) {
   app.post("/registr", async (req, res) => {
@@ -7,9 +17,10 @@ module.exports = function (app) {
       const { email, password } = req.body;
       const user = new User({ email, password });
       await user.save();
-      res.status(201).json(user);
+      const token = generateAccessToken({ name: email });
+      res.json(token);
     } catch (err) {
-      console / log(err);
+      console.log(err);
     }
   });
 };
